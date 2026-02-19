5ae7b185ab8cada63c944a1ec9d97437631ddd2e
Илья <50883558+Iliapopov91@users.noreply.github.com>
2026-02-19 17:33:17 +0300

    Update Test.java

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,6 +1,7 @@
 
 
     public class Test {
-        public static void main(String[] args) {System.out.println ("Hello lev!!!!");
+        private static String msg = ""Hello lev!!!!"";
+        public static void main(String[] args) {System.out.println (msg);
     }
 }
